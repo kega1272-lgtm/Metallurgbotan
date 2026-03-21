6dc7dd075a0570d0117cb03658775e078c358ff3
kega1272-lgtm <kega1272@gmail.com>
2026-03-21 17:25:40 +0500

    Update Test5.py

--- Test5.py
+++ Test5.py
@@ -19,7 +19,7 @@ from aiogram.fsm.storage.memory import MemoryStorage
 
 # ─────────────────────────── НАСТРОЙКИ ───────────────────────────
 
-TOKEN = "8756631618:AAFbU_ipn9JaQQwv5wILzy_jUnOXXEzvL0A"
+TOKEN = "8756631618:AAFCINR-Vv2Gapt4PmbNrnrZjbfJYvH8rHI"
 
 CHECK_INTERVAL = 30
 IDLE_INTERVAL = 120
